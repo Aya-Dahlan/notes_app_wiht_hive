caee3d0e79c45834c6e3e4892dbd25c823808c7d
Aya-Dahlan <ayaalidahlan@gmail.com>
2024-04-18 12:54:57 +0200

    create type adapter

--- lib/views/widgets/edit_view_body.dart
+++ lib/views/widgets/edit_view_body.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app_with_hive/views/widgets/custom_app_bar.dart';
+import 'package:notes_app_with_hive/views/widgets/custom_textLfield.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 50,
+          ),
+          CustomAppBar(
+            title: 'Edit Notes',
+            icon: Icons.check,
+          ), 
+          SizedBox(
+            height: 50,
+          ), 
+          CustomTextField(hint: 'Title'), 
+          SizedBox(
+            height: 16,
+          ), 
+          CustomTextField(hint: 'Content', maxLines: 5,)
+        ],
+      ),
+    );
   }
 }
